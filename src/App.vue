4ce606b7d9cf6c5dc070a9a629a3189604e268f6
peyman <peymannaderi3@yahoo.com>
2021-03-26 01:52:40 +0430

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <router-view />
+    <Sidebar />
+  </div>
+</template>
+
+<script>
+import Sidebar from "./components/Sidebar";
+
+export default {
+  name: "App",
+  components: {
+    Sidebar,
+  },
+};
+</script>
+
+<style lang="scss"></style>
