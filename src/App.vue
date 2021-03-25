011c15339d24ad66ff78cb1c2fa19059d8118aca
peyman <peymannaderi3@yahoo.com>
2021-03-26 01:59:21 +0430

    fix component structre and layout.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <template>
   <div>
     <router-view />
-    <Sidebar />
   </div>
 </template>
-
-<script>
-import Sidebar from "./components/Sidebar";
-
-export default {
-  name: "App",
-  components: {
-    Sidebar,
-  },
-};
-</script>
 
 <style lang="scss"></style>
---
+++ src/components/layout/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <Sidebar />
+  </div>
+</template>
+
+<script>
+import Sidebar from "../Sidebar";
+
+export default {
+  components: {
+    Sidebar,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
