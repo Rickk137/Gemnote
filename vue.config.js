4a71ea3e1a2a123e22641855b70fd08f9bf27f37
peyman <peymannaderi3@yahoo.com>
2021-03-27 15:28:56 +0430

    fix pwa config

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pwa: {
+    name: "Gemnote",
+    themeColor: "#24292c",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    workboxOptions: {
+      skipWaiting: true,
+    },
+  },
+};
